d6568cb383c203a34334a369f58333b93ab03158
matthewoso <matthewosofsky@matthews-MacBook-Pro.local>
2017-03-27 10:31:11 -0600

    Comments added

--- src/components/PointOfSale.vue
+++ src/components/PointOfSale.vue
@@ -1,25 +1,34 @@
 <template >
   <div class="hello">
+<!-- loop to set up series of buttons for each item -->
+
     <button
       v-for="(item, index) of inventory"
       v-on:click="addToCheck(item)"
     >
+
+<!--     bring in image associated with each item -->
       <img v-bind:src="getImage(item.image)" height="64">
       {{item.name}}
       ${{convertPrice(item.cost)}}
-
     </button>
+
+
     <p>
       <ul><u>Current Order</u> 
+<!--       loop to list out all selected items -->
         <li v-for="(item, index) of theOrder"
         >{{item.name }} ${{convertPrice(item.cost)}}
+        <!-- set up buttons to delete items -->
         <button v-on:click="removeFromCheck(index, item.cost)">x</button>
         </li>
       </ul>
+<!--       display computed amounts -->
       <ul>subTotal ${{convertPrice(orderTotal)}}</ul>
       <ul>Tax 8% = ${{getTax(orderTotal)}}</ul>
       <ul>Total  ${{getTotal(orderTotal)}}</ul>
     </p>
+    <!-- set up buttons to delete all -->
     <button v-on:click="deleteAll">Resest</button>
   </div>
   
@@ -41,11 +50,15 @@ export default {
     }
   },
   // props: ['inventory'],
+
   methods: {
+//easier to set up string to access image with a function
     getImage: function (x) {
       var strT = './static/' + x
       return strT
     },
+
+      // functions to compute amounts
     convertPrice: function (x) {
       var price = (x / 100).toFixed(2)
       return price
@@ -58,8 +71,11 @@ export default {
       var total = ((x + (x * 0.08)) / 100).toFixed(2)
       return total
     },
+
+    //functions to set items on check
+
     addToCheck: function (item) {
-      this.orderTotal = this.orderTotal + item.cost
+      this.orderTotal = this.orderTotal + item.cost. //this references for variables defined in data
       this.theOrder.push(item)
     },
     removeFromCheck: function (index, price) {
